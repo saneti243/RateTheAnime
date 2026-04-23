2861d2eaf3cf159fb1297374ce8ad227d3eddfa1
saneti243 <123591489+saneti243@users.noreply.github.com>
2026-04-23 12:17:43 -0400

    Add files via upload

---
+++ ratetheanime/src/main/java/com/duoc/ratetheanime/RatetheanimeApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ratetheanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatetheanimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatetheanimeApplication.class, args);
+	}
+
+}
---
+++ ratetheanime/src/test/java/com/duoc/ratetheanime/RatetheanimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.ratetheanime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatetheanimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
